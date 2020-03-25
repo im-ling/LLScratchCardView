14471010a76436385681b61ad7364122b5aa50ff
NowOrNever <tobeabetterman_dl@163.com>
2020-03-26 01:52:39 +1100

    feat: add pixel, blur demo, add custom pixel function

--- LLScratchCardView/UIImage+Extension.swift
+++ LLScratchCardView/UIImage+Extension.swift
@@ -37,7 +37,7 @@ extension UIImage {
     
     func blurImage(blurAmount: CGFloat) -> UIImage? {
         guard let ciImage = CIImage.init(image: self) else { return self }
-        let blurFilter = CIFilter(name: "CIGaussianBlur")
+        let blurFilter = CIFilter(name: "CIBoxBlur")
         blurFilter?.setValue(ciImage, forKey: kCIInputImageKey)
         blurFilter?.setValue(blurAmount, forKey: kCIInputRadiusKey)
         guard let outputImage = blurFilter?.outputImage else { return self }
@@ -52,5 +52,97 @@ extension UIImage {
         guard let outputImage = pixellateFilter?.outputImage else { return self }
         return UIImage.init(ciImage: outputImage)
     }
+    
+    func ll_pixelImage(level: Int) -> UIImage? {
+        let image = self
+        
+        guard let inputCGImage = image.cgImage else {
+            print("ll_pixelImage unable to get cgImage")
+            return self
+        }
+        let colorSpace       = CGColorSpaceCreateDeviceRGB()
+        let width            = inputCGImage.width
+        let height           = inputCGImage.height
+        let bytesPerPixel    = 4
+        let bitsPerComponent = 8
+        let bytesPerRow      = bytesPerPixel * width
+        let bitmapInfo       = RGBA32.bitmapInfo
+
+        guard let context = CGContext(data: nil, width: width, height: height, bitsPerComponent: bitsPerComponent, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo) else {
+            print("ll_pixelImage unable to create context")
+            return self
+        }
+        context.draw(inputCGImage, in: CGRect(x: 0, y: 0, width: width, height: height))
+
+        guard let buffer = context.data else {
+            print("ll_pixelImage unable to get context data")
+            return self
+        }
+        let pixelBuffer = buffer.bindMemory(to: RGBA32.self, capacity: width * height)
+        var pixel:RGBA32 = RGBA32.init(red: 0, green: 0, blue: 0, alpha: 0)
+        for row in 0 ..< Int(height) {
+            for column in 0 ..< Int(width) {
+                let offset = row * width + column
+                if row % level == 0 {
+                    if column % level == 0 {
+                        pixel = pixelBuffer[offset]
+                    }else{
+                        pixelBuffer[offset] = pixel
+                    }
+                }else{
+                    pixelBuffer[offset] = pixelBuffer[offset - width]
+                }
+            }
+        }
+
+        let outputCGImage = context.makeImage()!
+        let outputImage = UIImage(cgImage: outputCGImage, scale: image.scale, orientation: image.imageOrientation)
+        return outputImage
+    }
 }
+
+
+struct RGBA32: Equatable {
+    private var color: UInt32
+
+    var redComponent: UInt8 {
+        return UInt8((color >> 24) & 255)
+    }
+
+    var greenComponent: UInt8 {
+        return UInt8((color >> 16) & 255)
+    }
+
+    var blueComponent: UInt8 {
+        return UInt8((color >> 8) & 255)
+    }
+
+    var alphaComponent: UInt8 {
+        return UInt8((color >> 0) & 255)
+    }
+
+    init(red: UInt8, green: UInt8, blue: UInt8, alpha: UInt8) {
+        let red   = UInt32(red)
+        let green = UInt32(green)
+        let blue  = UInt32(blue)
+        let alpha = UInt32(alpha)
+        color = (red << 24) | (green << 16) | (blue << 8) | (alpha << 0)
+    }
+
+    static let red     = RGBA32(red: 255, green: 0,   blue: 0,   alpha: 255)
+    static let green   = RGBA32(red: 0,   green: 255, blue: 0,   alpha: 255)
+    static let blue    = RGBA32(red: 0,   green: 0,   blue: 255, alpha: 255)
+    static let white   = RGBA32(red: 255, green: 255, blue: 255, alpha: 255)
+    static let black   = RGBA32(red: 0,   green: 0,   blue: 0,   alpha: 255)
+    static let magenta = RGBA32(red: 255, green: 0,   blue: 255, alpha: 255)
+    static let yellow  = RGBA32(red: 255, green: 255, blue: 0,   alpha: 255)
+    static let cyan    = RGBA32(red: 0,   green: 255, blue: 255, alpha: 255)
+
+    static let bitmapInfo = CGImageAlphaInfo.premultipliedLast.rawValue | CGBitmapInfo.byteOrder32Little.rawValue
+
+    static func ==(lhs: RGBA32, rhs: RGBA32) -> Bool {
+        return lhs.color == rhs.color
+    }
+}
+
 
--- LLScratchCardViewDemo/LLScratchCardViewDemo/ViewController.swift
+++ LLScratchCardViewDemo/LLScratchCardViewDemo/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     var scratchView: LLScratchCardView?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,16 +19,18 @@ class ViewController: UIViewController {
     }
     
     func setupUI() {
-        guard let originalImage = UIImage.init(named: "tifa")?.ll_getAspectFillImage(imageViewSize: view.size),
-            let maskImage = UIImage.init(named: "aerith")?.ll_getAspectFillImage(imageViewSize: view.size) else { return }
-        scratchView = LLScratchCardView.init(frame: view.frame, originalImage: originalImage, maskImage: maskImage)
+        let frame = view.frame
+        guard let originalImage = UIImage.init(named: "tifa")?.ll_getAspectFillImage(imageViewSize: frame.size),
+            let maskImage = UIImage.init(named: "ff7")?.ll_getAspectFillImage(imageViewSize: frame.size) else { return }
+        scratchView = LLScratchCardView.init(frame: frame, originalImage: originalImage, maskImage: maskImage)
         view.addSubview(scratchView!)
 
         let tintColor = UIColor.colorWithHex(hex: 0x6A81F9)
         let undoBtn = UIButton.ll_button(title: "Undo", target: self, action: #selector(undoButtionClickAction(sender:)), color: tintColor)
         let resetBtn = UIButton.ll_button(title: "Reset", target: self, action: #selector(reset), color: tintColor)
-        let redoBtn = UIButton.ll_button(title: "Redo", target: self, action: #selector(redoClickAction(sender:)), color:  tintColor)
         let doneBtn = UIButton.ll_button(title: "Done", target: self, action: #selector(doneButtonClickAction(sender:)), color: tintColor)
+        let nextBtn = UIButton.ll_button(title: "Next", target: self, action: #selector(nextButtonClickAction(sender:)), color: tintColor)
+        let redoBtn = UIButton.ll_button(title: "Redo", target: self, action: #selector(redoClickAction(sender:)), color:  tintColor)
 
         let height:CGFloat = 45.0
         let toolBar = UIToolbar.init(frame: CGRect.init(x: 0, y: view.height - height - UIApplication.shared.statusBarFrame.size.height, width: view.width, height: height ))
@@ -37,8 +40,10 @@ class ViewController: UIViewController {
         barItems.append(UIBarButtonItem.init(customView: undoBtn))
         barItems.append(UIBarButtonItem.init(barButtonSystemItem: .flexibleSpace, target: nil, action: nil))
         barItems.append(UIBarButtonItem.init(customView: resetBtn))
-        barItems.append(UIBarButtonItem.init(customView: UIView.init(frame: CGRect.init(origin: CGPoint.zero, size: CGSize.init(width: 10, height: height)))))
+        barItems.append(UIBarButtonItem.init(barButtonSystemItem: .flexibleSpace, target: nil, action: nil))
         barItems.append(UIBarButtonItem.init(customView: doneBtn))
+        barItems.append(UIBarButtonItem.init(barButtonSystemItem: .flexibleSpace, target: nil, action: nil))
+        barItems.append(UIBarButtonItem.init(customView: nextBtn))
         barItems.append(UIBarButtonItem.init(barButtonSystemItem: .flexibleSpace, target: nil, action: nil))
         barItems.append(UIBarButtonItem.init(customView: redoBtn))
         toolBar.items = barItems
@@ -56,6 +61,32 @@ class ViewController: UIViewController {
         scratchView?.reset()
     }
     
+    
+    var index = 0
+    var imageName = ["aerith", "tifa", "yuffie"]
+    var coverImage = UIImage.init(named: "ff7")
+
+    @objc func nextButtonClickAction(sender: UIButton){
+        switch index % imageName.count {
+        case 0:
+            let image = UIImage.init(named: imageName[0])
+            scratchView?.originalImage = image?.ll_getAspectFillImage(imageViewSize: scratchView?.size)
+            scratchView?.maskImage = image?.ll_getAspectFillImage(imageViewSize: scratchView?.size)?.blurImage(blurAmount: 15)
+        case 1:
+            let image = UIImage.init(named: imageName[1])
+            scratchView?.originalImage = image?.ll_getAspectFillImage(imageViewSize: scratchView?.size)?.pixelImage(level: 20)
+            scratchView?.maskImage = image?.ll_getAspectFillImage(imageViewSize: scratchView?.size)
+        case 2:
+            let originalImage = UIImage.init(named: imageName[2])?.ll_getAspectFillImage(imageViewSize: scratchView?.size)
+            scratchView?.originalImage = originalImage
+            scratchView?.maskImage = coverImage
+        default:
+            return
+        }
+        scratchView?.reset()
+        index += 1
+    }
+
     @objc func doneButtonClickAction(sender: UIButton){
         let vc = ResultImageController.init(frame: view.frame)
         let snapshot = scratchView?.snapshot()
@@ -63,7 +94,6 @@ class ViewController: UIViewController {
         vc.view.backgroundColor = .gray
         present(vc, animated: true, completion: nil)
     }
-    
     
 }
 
